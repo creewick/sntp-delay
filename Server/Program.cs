4e06d9a8d4f5aa1334a829b1e2ed46500dd4e6ea
creewick <creewick@ya.ru>
2018-03-09 20:10:15 +0500

    Initial commit

---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server
+{
+    public static class Program
+    {
+        private static int[] DefaultSettings => new[] { 123, 1024, Environment.ProcessorCount };
+        
+        private static int[] GetSettings(string[] args)
+        {
+            var settings = DefaultSettings;
+            for (var i = 0; i < args.Length; i++)
+                if (int.TryParse(args[i], out var value))
+                    settings[i] = value;
+            return settings;
+        }
+        
+        public static void Main(string[] args)
+        {
+            var server = new SntpServer(GetSettings(args));
+            server.Start();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+
+namespace Server
+{
+    public class SntpServer
+    {
+        private readonly Socket Socket;
+        private readonly byte[] Buffer;
+        private readonly Queue<byte[]> Queue;
+        
+        public SntpServer(int[] settings) : this(settings[0], settings[1], settings[2]){}
+
+        public SntpServer(int port, int bufferSize, int threadsCount)
+        {
+            Socket = GetUdpSocket(port);
+            Buffer = new byte[bufferSize];
+            Queue = new Queue<byte[]>();
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                try
+                {
+                    var size = Socket.Receive(Buffer);
+                    Queue.Enqueue(Buffer.Take(size).ToArray());
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("{0} Error code: {1}.", e.Message, e.ErrorCode);
+                    return;
+                }
+
+            }
+        }
+        
+        private static Socket GetUdpSocket(int port)
+        {
+            var socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(
+                IPAddress.Loopback, 
+                port));
+            return socket;
+        }
+    }
+}
